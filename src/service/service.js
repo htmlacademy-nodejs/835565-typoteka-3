d21fa129d6eab6467eccc50c4609962e6000435d
bremxx <petr.kirienko@gmail.com>
2021-04-21 21:07:12 +0300

    точка входа

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {MockApp} = require(`../mock-app`);
+const {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  ExitCode
+} = require(`../mock-app/const`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+
+if (userArguments.length === 0 || !MockApp[userCommand]) {
+  MockApp[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+MockApp[userCommand].run(userArguments.slice(1));
