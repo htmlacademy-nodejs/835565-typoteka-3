dfc93ce0e98dd11dc4ec9f089a13e9f80f973d8c
bremxx <petr.kirienko@gmail.com>
2021-04-21 21:04:37 +0300

    модуль выполнения команды generate

---
+++ src/mock-app/generate.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const {
+  getRandomNum,
+  shuffle,
+  getRandomDate
+} = require(`./utils`);
+const fs = require(`fs`);
+
+const {
+  DEFAULT_COUNT,
+  FILE_NAME,
+  SentencesNum,
+  CategoriesNum,
+  TITLES,
+  TEXT,
+  CATEGORY,
+} = require(`./const`);
+
+const generateMockData = (count) => {
+  return Array(count).fill({}).map(() => ({
+    title: TITLES[getRandomNum(0, TITLES.length - 1)],
+    createdDate: getRandomDate(),
+    announce: shuffle(TEXT).slice(0, getRandomNum(SentencesNum.MIN, SentencesNum.MAX)).join(` `),
+    fullText: shuffle(TEXT).slice(0, getRandomNum(SentencesNum.MIN, TEXT.length - 1)).join(` `),
+    сategory: shuffle(CATEGORY).slice(0, getRandomNum(CategoriesNum.MIN, CategoriesNum.MAX)),
+  }));
+};
+
+module.exports = {
+  name: `--generate`,
+  run(args) {
+    const [count] = args;
+    const articlesCount = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    const articles = JSON.stringify(generateMockData(articlesCount));
+
+    fs.writeFile(FILE_NAME, articles, (err) => {
+      return (
+        err
+          ? console.error(`Can't write data to file...`)
+          : console.info(`Operation success. File created.`)
+      );
+    });
+  }
+};
