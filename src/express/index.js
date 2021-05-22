0868ddb903f9b9fd890d75d36cd125385cd1f714
bremxx <petr.kirienko@gmail.com>
2021-05-22 16:09:46 +0300

    подключение шаблонизатора

--- src/express/index.js
+++ src/express/index.js
@@ -1,16 +1,23 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const articlesRoutes = require(`./routes/articles-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
 app.use(`/`, mainRoutes);
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
