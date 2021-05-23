fc76f7317f3757fff23a8b2eef4fb5a9d23b20d5
bremxx <petr.kirienko@gmail.com>
2021-05-23 17:06:08 +0300

    создание основных шаблонов, подключение шаблонов

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,10 +3,9 @@
 const {Router} = require(`express`);
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (request, response) => response.send(`/`));
-mainRouter.get(`/register`, (request, response) => response.send(`/register`));
-mainRouter.get(`/login`, (request, response) => response.send(`/login`));
-mainRouter.get(`/search`, (request, response) => response.send(`/search`));
-mainRouter.get(`/categories`, (request, response) => response.send(`/categories`));
+mainRouter.get(`/`, (request, response) => response.render(`main`));
+mainRouter.get(`/register`, (request, response) => response.render(`registration`));
+mainRouter.get(`/login`, (request, response) => response.render(`login`));
+mainRouter.get(`/search`, (request, response) => response.render(`search`));
 
 module.exports = mainRouter;
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,7 +3,8 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (request, response) => response.send(`/my`));
-myRouter.get(`/comments`, (request, response) => response.send(`/my/comments`));
+myRouter.get(`/`, (request, response) => response.render(`my`));
+myRouter.get(`/comments`, (request, response) => response.render(`comments`));
+myRouter.get(`/categories`, (request, response) => response.render(`categories`));
 
 module.exports = myRouter;
