a227946c94e587c009656d5fa14dc3fc8ffd0058
bremxx <petr.kirienko@gmail.com>
2021-09-02 17:54:48 +0300

    условие для проверки на переменные окружения

--- src/service/lib/sequelize.js
+++ src/service/lib/sequelize.js
@@ -1,14 +1,17 @@
 'use strict';
 
 const Sequelize = require(`sequelize`);
+const {Env} = require(`../../const`);
 const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
 
 const envVariablesAreMissing = [
   DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 ].some((variable) => variable === undefined);
 
-if (envVariablesAreMissing) {
-  throw new Error(`Some environmental variables are not defined`);
+if (process.env.NODE_ENV === Env.DEVELOPMENT) {
+  if (envVariablesAreMissing) {
+    throw new Error(`Some environmental variables are not defined`);
+  }
 }
 
 module.exports = new Sequelize(
