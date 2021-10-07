3eadf492c70cf39f29aed0971261ff5d884a93f6
bremxx <petr.kirienko@gmail.com>
2021-10-07 21:19:23 +0300

    добавлена валидация входных данных через Joi

--- src/service/middlewares/comment-validator.js
+++ src/service/middlewares/comment-validator.js
@@ -1,15 +1,26 @@
 'use strict';
 
-const {HttpCode, defaultCommentKeys} = require(`../../const`);
+const Joi = require(`joi`);
+const {HttpCode} = require(`../../const`);
+
+const ErrorCommentMessage = {
+  TEXT: `Комментарий слишком короткий. Минимум 20 символов.`
+};
+
+const schema = Joi.object({
+  text: Joi.string().min(20).required().messages({
+    'string.min': ErrorCommentMessage.TEXT
+  })
+});
 
 module.exports = (req, res, next) => {
   const comment = req.body;
-  const currentCommentKeys = Object.keys(comment);
-  const keysMatches = defaultCommentKeys.every((key) => currentCommentKeys.includes(key));
 
-  if (!keysMatches) {
+  const {error} = schema.validate(comment, {abortEarly: false});
+
+  if (error) {
     return res.status(HttpCode.BAD_REQUEST)
-      .send(`Comment structure is invalid`);
+      .send(error.details.map((err) => err.message).join(`\n`));
   }
 
   return next();
