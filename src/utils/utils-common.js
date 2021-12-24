8db61776d20d033e838b991aeae957d5f688d5f3
bremxx <petr.kirienko@gmail.com>
2021-12-24 19:18:36 +0300

    косметические правки

--- src/utils/utils-common.js
+++ src/utils/utils-common.js
@@ -59,12 +59,12 @@ const adaptFormDataToClient = (data) => (
 
 const createDirs = (dirpaths) => {
   console.info(chalk.green(`Creating folders...`));
-  for (const dirpath of dirpaths) {
+  dirpaths.forEach((dirpath) => {
     console.info(chalk.green(`Creating folder: ${dirpath}`));
     fs.mkdirSync(path.resolve(process.cwd(), dirpath), {
       recursive: true,
     });
-  }
+  });
   console.info(chalk.green(`Finished. \n`));
 };
 
